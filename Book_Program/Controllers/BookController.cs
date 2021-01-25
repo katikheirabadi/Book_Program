abf03d677fc3b64a0ea23526bf342909d8b9b9ad
ASUS <kati.kheirabadi@gmail.com>
2021-01-25 19:28:31 +0330

    aa

--- Book_Program/Controllers/BookController.cs
+++ Book_Program/Controllers/BookController.cs
@@ -50,22 +50,62 @@ namespace Book_Program.Controllers
             return end;
 
         }
-       // [HttpPost("Search")]
-        //public OUTPUT_BOOKLIST Search(search_model search)
-        //{
-        //    var allbooks = repository.GetAll();
-        //    var endlist = new OUTPUT_BOOKLIST();
-        //    if (search.publication != null)
-        //    {
-        //        endlist.books = allbooks.Where(b => b.Publication.Name == search.publication).ToList();
-        //    }
-        //    if(search.categories!=null)
-        //    {
+        [HttpPost("Search")]
+        public OUTPUT_BOOKLIST Search(search_model search)
+        {
+            var allbooks = repository.GetAll();
+            var endList = new OUTPUT_BOOKLIST();
+           
+            if (search.publication != null)
+            {
+                endList.books = allbooks.Where(b => b.Publication.Name == search.publication).ToList();
+            }
+            if (search.categories != null)
+            {
+                var bookcategory = new List<List<Book_Catrgiry>>();
+                foreach (var book in allbooks)
+                    bookcategory.Add(book.book_Catrgiries);
 
-        //    }
+                for(int i=0; i<bookcategory.Count;i++)
+                {
+                    for(int j=0;j<bookcategory[i].Count;j++)
+                    {
+                        var categoryname = bookcategory[i][j].Category.Name;
+                        if (search.categories.Contains(categoryname))
+                        {
+                            var exist = allbooks.Where(b => b.book_Catrgiries.Contains(bookcategory[i][j])).ToList();
+                            foreach (var book in exist)
+                                if (!endList.books.Contains(book))
+                                    endList.books.Add(book);
+                        }
 
+                    }
+                }
+            }
+            if(search.authors != null)
+            {
+                var boocauthor = new List<List<Author_Book>>();
+                foreach (var book in allbooks)
+                    boocauthor.Add(book.author_Books);
 
-        //    return endlist;
-        //}
+                for (int i = 0; i < boocauthor.Count; i++)
+                {
+                    for (int j = 0; j < boocauthor[i].Count; j++)
+                    {
+                        var authorname = boocauthor[i][j].Author.FullName;
+                        if (search.authors.Contains(authorname))
+                        {
+                            var exist = allbooks.Where(b => b.author_Books.Contains(boocauthor[i][j])).ToList();
+                            foreach (var book in exist)
+                                if (!endList.books.Contains(book))
+                                    endList.books.Add(book);
+                        }
+
+                    }
+                }
+            }
+
+            return endList;
+        }
     }
 }
